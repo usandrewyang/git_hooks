1be44be02ecda86d83199a15bbbc7e94e6ba2546
Andrew Yang <andrew.yang@broadcom.com>
2024-10-23 15:39:08 -0700

    Fix bugs

--- hooks/check_release_files.py
+++ hooks/check_release_files.py
@@ -1,10 +1,36 @@
 #!/tools/bin/python
 import sys
 
-def check_file_exist(file_path):
-    print(f'{file_path}')
-    return 0
+def check_file_exist(file_path, files_list):
+    print(f'checking {file_path} in {files_list}')
+    with open(files_list, "r") as file:
+        paths = file.readlines()
+
+    # Check if the target path exists in the file
+    if file_path in [line.strip() for line in paths]:
+        print(f"{file_path} exists in {files_list}")
+        return 1
+    else:
+        print(f"{file_path} does not exist in {files_list}")
+        return 0
 
 def main():
+    file_not_found = 0
+
+    print(f'checking FILES.ltsw')
     for file_path in sys.argv[1:]:
-        check_file_exist(file_path)
+        if not check_file_exist(file_path, "FILES.ltsw"):
+            file_not_found = 1
+            break
+
+    if not file_not_found:
+        return file_not_found
+
+    file_not_found = 0
+    print(f'checking FILES.esw')
+    for file_path in sys.argv[1:]:
+        if not check_file_exist(file_path, "FILES.esw"):
+            file_not_found = 1
+            break
+
+    return file_not_found
