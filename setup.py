1725afe03902e66244e547e3063e75acee242627
Andrew Yang <andrew.yang@broadcom.com>
2024-10-23 09:38:57 -0700

    Fallback setup.py to default

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="remove_cpp_comments",
-    version="0.1",
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'remove-cpp-comments=hooks.remove_cpp_comments:remove_cpp_comments',
-        ],
-    },
-)
+setup()
 
